6d3430ea4427b5bd901657419db200acbcd52af8
Ahmed Ragab <a7mdragab1@gmail.com>
2026-04-16 13:14:53 +0200

    Initial fork of the original GetX

---
+++ lib/super_get.dart
@@ -0,0 +1,6 @@
+/// SuperGet is a fork of GetX - an extra-light and powerful multiplatform framework.
+/// It combines high performance state management, intelligent dependency
+/// injection, and route management in a quick and practical way.
+library super_get;
+
+export 'get.dart';
